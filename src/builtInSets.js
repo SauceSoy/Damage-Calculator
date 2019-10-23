a03553095e178de6a22fdab838e66fae0b21cfd7
SauceSoy <ahmedsiad114@gmail.com>
2019-10-22 20:20:16 -0600

    luxoar

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -272,6 +272,40 @@ var builtInSets = [
     },
 
     {
+        name: "Luxoar",
+        setName: "Wallbreaker",
+        evs: {
+            hp: 100,
+            energy: 0,
+            attack: 0,
+            defense: 0,
+            attackR: 200,
+            defenseR: 0,
+            speed: 200
+        },
+        ivs: {
+            hp: 40,
+            energy: 40,
+            attack: 40,
+            defense: 40,
+            attackR: 40,
+            defenseR: 40,
+            speed: 40
+        },
+        moves: {
+            move1: "Raging Howl",
+            move2: "Luminous Roar",
+            move3: "Scapegoat",
+            move4: "Disperse"
+        },
+        posNature: "nimble",
+        negNature: "none",
+        ability: "Illuminate",
+        item: "Light Essence",
+        level: 50
+    },
+
+    {
         name: "Avitross",
         setName: "Nosedive",
         evs: {
