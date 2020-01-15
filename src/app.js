85a405f3dc20b641244f57e8b5c4671baab2d4f1
SauceSoy <ahmedsiad114@gmail.com>
2020-01-14 21:58:36 -0700

    small fix

--- src/app.js
+++ src/app.js
@@ -214,7 +214,7 @@ $(document).ready(load);
 
 function load() {
     loadDropdowns();
-    if (document.cookie != "a") {
+    if (document.cookie != "") {
         let cookRaw = getCookie("setData");
         let cook = cookRaw.substring(8, cookRaw.length);
         let seenChangelongCookie = getCookie("changelog2").substring(11);
