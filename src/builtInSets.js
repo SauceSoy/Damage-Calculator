02cd8b94dcf901611bfa568c6afc19bfea0cfe33
GOATMaxwellN <66809914+GOATMaxwellN@users.noreply.github.com>
2020-06-16 00:30:03 -0500

    Adding a Boarrok set
    Hopefully this is formatted right. I wasn't sure how to add two negative natures, so hopefully I did it in a way that works.

--- src/builtInSets.js
+++ src/builtInSets.js
@@ -1,3 +1,36 @@
 var builtInSets = [
+     {
+        name: "Boarrok",
+        setName: "+1 Sweeper",
+        evs: {
+            hp: 176,
+            energy: 4,
+            attack: 200,
+            defense: 0,
+            attackR: 200,
+            defenseR: 0,
+            speed: 120
+        },
+        ivs: {
+            hp: 40,
+            energy: 40,
+            attack: 40,
+            defense: 40,
+            attackR: 40,
+            defenseR: 40,
+            speed: 40
+        },
+        moves: {
+            move1: "Earthquake",
+            move2: "Mega Chomp",
+            move3: "Raging Tackle",
+            move4: "Quick Pounce"
+        },
+        posNature: "very brawny",
+        negNature: ["clumsy", "tender"],
+        ability: "Boast",
+        item: "Earth Essence",
+        level: 50
+    }
     
 ];
